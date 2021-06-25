95633d30a9b31bc4885a2903d52f9892043ebeca
Abbas Moharami <abbasmdev@gmail.com>
2021-06-25 12:10:07 +0430

    1. Added react-router-dom for routing. 2. created Home, Auth pages. 3. Configured routing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
-      <h2>Build Spotify clone</h2>
+      <Switch>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Auth/index.js
@@ -0,0 +1,5 @@
+function Auth() {
+  return <div>Login page</div>;
+}
+
+export default Auth;
