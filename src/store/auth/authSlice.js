6b59359e3a24cbe1ec711f3a1b8f8a76de6bf1be
Abbas Moharami <abbasmdev@gmail.com>
2021-06-25 13:31:07 +0430

    1. Add and config redux. 2. Create auth slice.

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    access_token: null,
+    token_type: null,
+    expires_in: null,
+  },
+  reducers: {
+    setLoginData(state, action) {
+      state.access_token = action.payload.access_token;
+      state.token_type = action.payload.token_type;
+      state.expires_in = action.payload.expires_in;
+    },
+    clearLoginData(state) {
+      state.access_token = null;
+      state.token_type = null;
+      state.expires_in = null;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+export default authSlice.reducer;
