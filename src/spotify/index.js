9169290b3259d80c0d8270faa5f10ab4e0ef14dc
Abbas Moharami <abbasmdev@gmail.com>
2021-06-29 12:17:10 +0430

    Updated spotifyRedirectUri to  release url.

--- src/spotify/index.js
+++ src/spotify/index.js
@@ -2,7 +2,7 @@ import SpotifyWebApi from "spotify-web-api-js";
 
 export const spotifyAuthEndpoint = "https://accounts.spotify.com/authorize";
 
-export const spotifyRedirectUri = "http://localhost:3000/auth/callback";
+export const spotifyRedirectUri = "https://spotify.abbasmdev.ir/auth/callback";
 
 export const spotifyClientId = "b9c843b422fd4517b08a3aa4f0dc8da5";
 
